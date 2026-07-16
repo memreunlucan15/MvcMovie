741119fa180d6d467984825db9ca0c102b095f87
memreunlucan15 <mehmetemreunlucan@gmail.com>
2026-07-16 12:42:14 +0300

    Baslangic: MVC iskeleti-Scaffolding-Filtreleme

---
+++ Controllers/HelloWorld.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
